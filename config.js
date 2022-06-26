b3593fe4a2707fb58b47c41b4f5b1d7e34295cc3
Yatheshvar <64597185+Yatheshvar@users.noreply.github.com>
2022-06-26 12:55:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+ apiKey: "AIzaSyCSO8xyCclCVX0kpi8Y_2YCNu8djsLpMz0",
+  authDomain: "voting-app-c33ef.firebaseapp.com",
+  projectId: "voting-app-c33ef",
+  storageBucket: "voting-app-c33ef.appspot.com",
+  messagingSenderId: "565477554260",
+  appId: "1:565477554260:web:9e1b062419ad4f817795c1"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
